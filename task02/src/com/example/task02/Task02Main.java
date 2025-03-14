31c951fe60a00e9bdd22e0237b1932122616a9c3
Max Razin <razinmax02@gmail.com>
2025-03-14 20:00:18 +0500

    Task02 complete

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -6,18 +6,26 @@ public class Task02Main {
 
     public static void main(String[] args) {
 
-        /*
+
         cycleGrayCode(2)
                 .limit(10)
                 .forEach(System.out::println);
-        */
 
+        System.out.println("---");
+
+        cycleGrayCode(3)
+                .limit(10)
+                .forEach(System.out::println);
     }
 
     public static IntStream cycleGrayCode(int n) {
+        if (n < 1 || n > 16) {
+            throw new IllegalArgumentException("Invalid bit rate.");
+        }
 
-        return null; // your implementation here
+        int size = (int) Math.pow(2, n);
 
+        return IntStream.iterate(0, code -> (code + 1) % size)
+                .map(code -> code ^ (code >> 1));
     }
-
 }
