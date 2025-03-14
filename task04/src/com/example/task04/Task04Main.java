d411a85c48baef9f9db2e86a5cc3d1cbb85ebbb6
Max Razin <razinmax02@gmail.com>
2025-03-14 20:40:48 +0500

    Task04 complete

--- task04/src/com/example/task04/Task04Main.java
+++ task04/src/com/example/task04/Task04Main.java
@@ -1,11 +1,28 @@
 package com.example.task04;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Task04Main {
-
     public static void main(String[] args) {
+        String result = new BufferedReader(new InputStreamReader(System.in, StandardCharsets.UTF_8))
+                .lines()
+                .flatMap(line -> Arrays.stream(line.toLowerCase().split("[^\\p{L}\\p{Nd}]+")))
+                .filter(word -> !word.isEmpty())
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .entrySet().stream()
+                .sorted(Comparator.<Map.Entry<String, Long>>comparingLong(Map.Entry::getValue)
+                        .reversed()
+                        .thenComparing(Map.Entry::getKey))
+                .limit(10)
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining("\n"));
 
-        // your implementation here
-
+        System.out.print(result + "\n"); // Явно добавляем \n для совместимости
     }
-
 }
